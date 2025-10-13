c251ecaaa87a2016752831bb8573dc1e65d829b7
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-10-13 13:24:21 -0300

    feat(#53): Puxando períodos já inseridos no banco

---
+++ src/utils/get_periodos_filter.py
@@ -0,0 +1,20 @@
+import requests
+from src.config import parameters
+
+def get_periodos_filter(dia,semana,mes,ano):
+    url = f"{parameters.BACK_BASE_URL}/dim-periodo/filter"
+    params = {
+        "dia": dia,
+        "semana": semana,
+        "mes": mes,
+        "ano": ano
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code in (200,201):
+        resposta = response.json()
+        print(f"Resposta Filtro {resposta}")
+        return resposta
+    else:
+        print(f"Erro ao chamar endpoint {url}: {response.status_code}, {response.content}")
+        return ""
