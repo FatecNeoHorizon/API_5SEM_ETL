e2439b0ad0555fed35fb66a152c6278723955fed
Cesar Pelogia <pelogia.cesar@hotmail.com>
2025-10-15 23:14:42 -0300

    refactor(#55): ajusta fluxo principal do ETL
    - Corrige chamadas para funções renomeadas, como `extrair_atividades`.
    - Adiciona chamadas para carregar atividades e períodos.
    - Organiza imports e remove duplicidades.
    - Melhora a organização do fluxo principal do ETL.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.extract import extract_atividades
 from src.extract import extract_periodos
 from src.load import load_projetos
 from src.load import load_periodos
+from src.load import load_atividades
 
 def main():
     """main."""
@@ -15,18 +16,21 @@ def main():
     print(f'JIRA_USERNAME: {parameters.JIRA_USERNAME}')
     print(f'JIRA_PASSWORD: {parameters.JIRA_PASSWORD}')
 
-    projetos = extract_projetos.extrair_todos_projetos()
+    projetos = extract_projetos.extrair_projetos()
     load_projetos.carregar_projetos(projetos)
 
-    atividades = extract_atividades.extract_atividades(projetos)
+    atividades = extract_atividades.extrair_atividades(projetos)
+    load_atividades.carregar_atividades(atividades)
+
+    periodos = extract_periodos.extract_periodos(atividades)
+    load_periodos.carregar_periodos(periodos)
 
     #Para os métodos de extração / carga das próximas dimensões,
     #Basta criar um método passando a variável 'atividades' como parâmetro
     #Exemplo:   devs = extract_devs(atividades) 
     #           carregar_devs(devs)
 
-    periodos = extract_periodos.extract_periodos(atividades)
-    load_periodos.carregar_periodos(periodos)
+    
 
 if __name__ == "__main__":
     main()
