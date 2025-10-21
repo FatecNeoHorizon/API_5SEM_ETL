452b5866cdb07157f2a004089acf73be7f86771d
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-10-20 21:44:54 -0300

    refactor(#56): Criando métodos reutilizáveis para retorno de objetos dimensões

--- src/extract/extract_fato_atividades.py
+++ src/extract/extract_fato_atividades.py
@@ -1,6 +1,6 @@
 import logging
 from src.utils.convert_datetime_to_periodo import convert_datetime_to_periodo
-from src.utils.get_periodos_filter import get_periodos_filter
+from src.utils import retorno_dimensoes
 
 logger = logging.getLogger(__name__)
 
@@ -20,16 +20,14 @@ def extrair_todos_fatos_atividades(jira_issues, atividades, projetos, status_arr
 
         for jira_issue in jira_issues:
 
-        
-
             atividade_jira_id = jira_issue["id"]
-            atividade = retornar_dim_atividade(atividades,atividade_jira_id)
+            atividade = retorno_dimensoes.retornar_dim_atividade(atividades,atividade_jira_id)
             if not atividade:
                 logger.warning("Erro ao extrair fato_atividade: Atividade não encontrada")
             
 
             projeto_jira_id = jira_issue["fields"]["project"]["id"]
-            projeto = retornar_dim_projeto(projetos, projeto_jira_id)
+            projeto = retorno_dimensoes.retornar_dim_projeto(projetos, projeto_jira_id)
             if not projeto:
                 logger.warning("Erro ao extrair fato_atividade: Projeto não encontrada")
 
@@ -45,17 +43,17 @@ def extrair_todos_fatos_atividades(jira_issues, atividades, projetos, status_arr
                 mes = 12,
                 ano = 99 )
 
-            periodo = retornar_dim_periodo(filtro_periodo)
+            periodo = retorno_dimensoes.retornar_dim_periodo(filtro_periodo)
             if not periodo:
                 logger.warning("Erro ao extrair fato_atividade: Período não encontrado")
 
             statusJiraId = jira_issue["fields"]["statusCategory"]["id"]
-            status = retornar_dim_status(status_array, statusJiraId)
+            status = retorno_dimensoes.retornar_dim_status(status_array, statusJiraId)
             if not status:
                 logger.warning("Erro ao extrair fato_atividade: Status não encontrado")
 
             tipoJiraId = jira_issue["fields"]["issuetype"]["id"]
-            tipo = retornar_dim_tipo(tipos, tipoJiraId)
+            tipo = retorno_dimensoes.retornar_dim_tipo(tipos, tipoJiraId)
             if not tipo:
                 logger.warning("Erro ao extrair fato_atividade: Tipo não encontrado")
 
@@ -82,35 +80,3 @@ def extrair_todos_fatos_atividades(jira_issues, atividades, projetos, status_arr
     except Exception as e:
         logger.warning("Erro ao fato_atividade: %s", e)
         return[]
-    
-
-def retornar_dim_atividade(atividades,atividade_jira_id):
-    for atividade in atividades:
-        if atividade['atividade_jira_id'] == atividade_jira_id:
-            return atividade
-    return None
-        
-def retornar_dim_projeto(projetos,projeto_jira_id):
-    for projeto in projetos:
-        if projeto['projeto_jira_id'] == projeto_jira_id:
-            return projeto
-    return None
-
-def retornar_dim_periodo(filtro_periodo):
-    banco_periodos = get_periodos_filter(filtro_periodo['dia'], filtro_periodo['semana'], filtro_periodo['mes'], filtro_periodo['ano'])
-    if len(banco_periodos) > 0:
-        return banco_periodos[0]
-    
-    return None
-
-def retornar_dim_status(status_array, statusJiraId):
-    for status in status_array:
-        if status['statusJiraId'] == statusJiraId:
-            return status
-    return None
-
-def retornar_dim_tipo(tipos, tipoJiraId):
-    for tipo in tipos:
-        if tipo['tipoJiraId'] == tipoJiraId:
-            return tipo
-    return None
